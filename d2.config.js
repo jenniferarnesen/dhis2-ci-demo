122f481f874e0a87597225e5689662aef20a00c7
Birk Johansson <birk@dhis2.org>
2023-08-10 11:26:08 +0200

    chore: add workflow

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,7 @@
 const config = {
+    id: '4d4fcc2e-2641-451d-9b65-cc748bfba8b3',
     type: 'app',
-    name: 'Ci App Demo',
+    title: 'CI App Demo',
     entryPoints: {
         app: './src/App.js',
     },
