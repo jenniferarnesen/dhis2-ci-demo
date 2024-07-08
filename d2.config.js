a61b8d4094af5fbc2e7a83bf63d4d1c158308ea5
Jen Jones Arnesen <jennifer@dhis2.org>
2024-07-08 16:14:44 +0200

    fix: maybe apphub token was missing

--- d2.config.js
+++ d2.config.js
@@ -1,11 +1,11 @@
 const config = {
-    id: '4d4fcc2e-2641-451d-9b65-cc748bfba8b3',
-    type: 'app',
-    title: 'CI App Demo',
-    entryPoints: {
-        app: './src/App.js',
-    },
-    minDHIS2Version: '2.35',
-}
+  id: "4d4fcc2e-2641-451d-9b65-cc748bfba8b3",
+  type: "app",
+  title: "CI App Demo",
+  entryPoints: {
+    app: "./src/App.js",
+  },
+  minDHIS2Version: "2.40",
+};
 
 module.exports = config
