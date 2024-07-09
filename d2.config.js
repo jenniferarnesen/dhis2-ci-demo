01d7e10714dfc27fe39ebe6bdb10fad7a9aa616a
Jen Jones Arnesen <jennifer@dhis2.org>
2024-07-09 10:17:27 +0200

    fix: add new app id

--- d2.config.js
+++ d2.config.js
@@ -1,6 +1,7 @@
 const config = {
   type: "app",
-  title: "CI App Demo",
+  id: "ea2d2384-3670-4769-9650-0c55df28b81f",
+  title: "Jennifer CI Test",
   entryPoints: {
     app: "./src/App.js",
   },
