5a06876f4df8acbd902dbc7e8d50f1c085d69586
Jen Jones Arnesen <jennifer@dhis2.org>
2024-07-09 12:03:00 +0200

    fix: use token instead of apikey

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const MyApp = () => (
                         name: data.me.name,
                       })}
                     </h1>
-                    <h3>"Welcome to DHIS2"</h3>
+                    <h3>"Welcome back"</h3>
                   </>
                 );
             }}
